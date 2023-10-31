ddada6e494388d51d7fd9aa1bdcaf9cc104eea33
natanael <nathaxx09@hotmail.com>
2023-10-30 22:55:46 -0300

    media q modificado

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const btnMenu = document.getElementById('btn__menu')
+const menu = document.getElementById('menu')
+btnMenu.addEventListener('click', ()=>{
+    btnMenu.classList.toggle('rotate')
+    menu.classList.toggle('activo')
+})
